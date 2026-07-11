75bc91ba47e0d9766de38c8efc37ae3601fcab95
Aman Singh Kushwaha <amankushwaha2001@gmail.com>
2026-07-11 09:53:50 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        for(let i=0; i<nums.length; i++) {
+            let index = Math.abs(nums[i]);
+            if(nums[index] < 0 ) return index;
+            nums[index] = -nums[index];
+        }
+    }
+}
